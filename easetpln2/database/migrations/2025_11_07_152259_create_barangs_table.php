65fef4f4e2cd2ed5767ad6dcfc134ce760c27726
Fathinah Nur Jannah <nurjannahfathinah@gmail.com>
2025-11-07 23:39:50 +0800

    feat: add columns in table Barang

--- easetpln2/database/migrations/2025_11_07_152259_create_barangs_table.php
+++ easetpln2/database/migrations/2025_11_07_152259_create_barangs_table.php
@@ -13,6 +13,21 @@ return new class extends Migration
     {
         Schema::create('barangs', function (Blueprint $table) {
             $table->id();
+            $table->unsignedInteger('No.');
+            $table->unsignedInteger('No. Reg');
+            $table->string('Nama Barang');
+            $table->foreignId('Unit')->constrained('ruangans')->onDelete('cascade');
+            $table->foreignId('Ruangan')->constrained('ruangans')->onDelete('cascade');
+            $table->enum('Status', ['Baik', 'Rusak', 'Perlu Diperbaiki'])->default('Baik');
+            $table->enum('progress_aksi', [
+                'Tidak Ada',
+                'Perlu Diganti',
+                'Dibuang',
+                'Belum Ditindak',
+                'Sementara Diperbaiki'
+            ])->default('Tidak Ada');
+            $table->string('Deskripsi')->nullable();
+            $table->enum('Urgensi', ['🔴', '🟡', '🟢'])->default('🟢');
             $table->timestamps();
         });
     }
