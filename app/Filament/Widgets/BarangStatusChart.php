0f8c368de01271c35636708e9cb1380023718020
Fathinah Nur Jannah <nurjannahfathinah@gmail.com>
2025-11-16 10:09:07 +0800

    style: adjust pie chart height to match bar chart

--- app/Filament/Widgets/BarangStatusChart.php
+++ app/Filament/Widgets/BarangStatusChart.php
@@ -10,7 +10,7 @@ use Filament\Widgets\PieChartWidget;
 class BarangStatusChart extends PieChartWidget
 {
     protected ?string $heading = 'Grafik Status Barang Saat Ini';
-    protected ?string $maxHeight = '250px';   // tinggi chart
+    protected ?string $maxHeight = '265px';   // tinggi chart
 
     // Untuk menampilkan filter Unit & Ruangan di atas chart
     protected function getFormSchema(): array
