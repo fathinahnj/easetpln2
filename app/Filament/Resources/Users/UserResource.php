b6ae97e58f9e14989cbd56b95070b3601e2f84e8
Fathinah Nur Jannah <nurjannahfathinah@gmail.com>
2025-11-16 17:49:02 +0800

    style: change style in Manajemen User navbar

--- app/Filament/Resources/Users/UserResource.php
+++ app/Filament/Resources/Users/UserResource.php
@@ -18,9 +18,10 @@ class UserResource extends Resource
 {
     protected static ?string $model = User::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedUsers;
 
     protected static ?string $recordTitleAttribute = 'Manajemen User';
+    protected static ?string $navigationLabel = 'Manajemen User';
 
     public static function form(Schema $schema): Schema
     {
